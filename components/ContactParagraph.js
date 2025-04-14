e1ce389aa0b5805589dbc325acad794ad090db18
mushfikahossain <itsmushfikahossain@gmail.com>
2025-04-14 18:00:47 -0400

    Update ContactParagraph.js
    Updated colours and font size

--- components/ContactParagraph.js
+++ components/ContactParagraph.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ContactParagraph = () => {
     return (
         <div style={{display:"block", justifyContent: "center"}}>
-            <p style={{textAlign:"center", padding:"10px", color:"#111111", marginLeft:"400px", marginRight:"400px"}}>For all inquiries, please feel free to contact us here. We will get back to you as soon as possible!</p>
+            <p style={{textAlign:"center", padding:"10px", color:"#1C0221", marginLeft:"400px", marginRight:"400px", fontSize: 25}}>For all inquiries, please feel free to contact us here. We will get back to you as soon as possible!</p>
         </div>
     );
 };
