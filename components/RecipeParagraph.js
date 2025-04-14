359811596a2e853ec88ac720a1637012a1c809b4
mushfikahossain <itsmushfikahossain@gmail.com>
2025-04-14 18:04:01 -0400

    Update RecipeParagraph.js

--- components/RecipeParagraph.js
+++ components/RecipeParagraph.js
@@ -1,29 +1,45 @@
 import React from 'react';
-
 import { useFavorites } from "@/components/FavoritesContext";
 
-
 export default function RecipeParagraph() {
-    const { addToFavorites } = useFavorites();
+  const { addToFavorites } = useFavorites();
 
-    const recipe = {
-        id: 1,
-        title: "Spaghetti Bolognese",
-        description: "A classic Italian pasta dish with a rich meat sauce.",
-    };
+  const recipes = [
+    {
+      id: 1,
+      title: "Spaghetti Bolognese",
+      description: "A classic Italian pasta dish with a rich meat sauce.",
+    },
+    {
+      id: 2,
+      title: "Chicken Alfredo",
+      description:
+        "Creamy fettuccine pasta with grilled chicken in a smooth alfredo sauce.",
+    },
+    {
+      id: 3,
+      title: "Cheeseburger",
+      description:
+        "A classic with a beef patty topped with melted cheese",
+    },
+  ];
 
-    function alertMSG() {
-        addToFavorites(recipe);
-        alert("Recipe added to favorites!");
-    }
-
-    return (
-        <section>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.description}</p>
-            <button onClick={alertMSG}>
-                Add to Favorites
-            </button>
-        </section>
-    );
+  return (
+    <section>
+      {recipes.map((recipe) => (
+        <article key={recipe.id}>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.description}</p>
+          <button
+            onClick={() => {
+              addToFavorites(recipe);
+              alert("Recipe added to favorites!");
+            }}
+          >
+            Add to Favorites
+          </button>
+        </article>
+      ))}
+    </section>
+  );
 }
