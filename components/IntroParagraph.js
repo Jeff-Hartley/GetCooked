52f46229eb940626f711c9bd2f2227c16912ca5c
mushfikahossain <itsmushfikahossain@gmail.com>
2025-04-14 18:03:09 -0400

    Update IntroParagraph.js
    Updated colours and font size

--- components/IntroParagraph.js
+++ components/IntroParagraph.js
@@ -4,7 +4,7 @@ import React from 'react';
 const IntroParagraph = () => {
     return (
         <div style={{display:"block", justifyContent: "center"}}>
-            <p style={{textAlign:"center", padding:"30px", color:"#111111", marginLeft:"400px", marginRight:"400px"}}>Welcome to our Recipe Book website.</p>
+            <p style={{textAlign:"center", padding:"30px", color:"#1C0221", marginLeft:"400px", marginRight:"400px", fontSize:35}}>Welcome to our Recipe Book website!</p>
         </div>
     );
 };
